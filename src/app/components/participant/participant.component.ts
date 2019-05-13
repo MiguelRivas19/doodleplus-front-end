a467a7eb7ab2cc4ba7f4aee41d96b041bf3f955d
Miguel Rivas <miguel.rivas.1189@gmail.com>
2019-05-13 16:15:25 +0200

    Test5 commit

--- src/app/components/participant/participant.component.ts
+++ src/app/components/participant/participant.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DoodleshareService } from '../../services/doodleshare.service';
+import { DoodleapiService } from '../../services/doodleapi.service';
 
 @Component({
   selector: 'app-participant',
@@ -8,12 +9,21 @@ import { DoodleshareService } from '../../services/doodleshare.service';
   styleUrls: ['./participant.component.css']
 })
 export class ParticipantComponent implements OnInit {
-
+  prenom: string;
+  nom: string;
+  email: string;
+  emails: Array<string>;
+ 
   constructor(
-   private doodleshare: DoodleshareService
+   private doodleshare: DoodleshareService,
+   private doodleapi: DoodleapiService
   ) { }
 
   ngOnInit() {
+  }
+
+  load(){
+   let c: any = this.doodleapi.postTest();
   }
 
 }
--- src/app/services/doodleapi.service.ts
+++ src/app/services/doodleapi.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Createur } from '../classes/createur';
+
 let url = "http://localhost:4200/api";
 
 @Injectable({
@@ -22,6 +24,26 @@ export class DoodleapiService {
   getAllergies(){
    let context: string = url + "/allergie"; 
    return this.get(context);
+  }
+
+  post(url:string, test: string){
+
+   let c: Createur = {
+    prenom: "miguel",
+    nom: "rivas",
+    email: "angel.rivas@gmail.com"
+   }
+
+   this.http.post(url, c)
+   .subscribe(
+    res => { console.log(res); },
+    err => { console.log("Error ocurrido, " + err)}
+   );
+  }
+
+  postTest(){
+   let context: string = url + "/test";
+   return this.post(context, "hola");
   }
 
 }
