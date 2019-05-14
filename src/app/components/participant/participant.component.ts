bc19645af185c8c23967a7f0892beb8809dfeb24
Miguel Rivas <miguel.rivas.1189@gmail.com>
2019-05-15 00:10:13 +0200

    Test7 commit

--- src/app/components/participant/participant.component.ts
+++ src/app/components/participant/participant.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 import { DoodleshareService } from '../../services/doodleshare.service';
 import { DoodleapiService } from '../../services/doodleapi.service';
@@ -13,15 +15,22 @@ export class ParticipantComponent implements OnInit {
   nom: string;
   email: string;
   participants: string;
- 
+  private _success = new Subject<string>(); 
+  successMessage: string;
+
   constructor(
    private doodleshare: DoodleshareService
   ) { }
 
   ngOnInit() {
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
   }
 
   load(){
+   this._success.next(`Merci pour votre preference`);
    let participants: Array<string> = this.participants.replace(/\s/g, "").split(',');
    let createur = {prenom: this.prenom, nom: this.nom, email: this.email};
    this.doodleshare.setCreateur(createur);
